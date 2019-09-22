b97f8691dc5b6b9487e0ed470ef034906f53c42d
mcosnta000 <mconsta000@gmail.com>
2019-09-22 09:44:57 -0400

    Implement challenge calculate action on Party

--- calc/urls.py
+++ calc/urls.py
@@ -12,5 +12,7 @@ router.register(r'parties', views.PartiesViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+#    path('challenge/party/<int:party>/encounter/<int:encounter>/', views.calculate_challenge, name='challenge'),
+    path('parties/<int:pk>/encounter/<int:encounter_id>/', views.PartiesViewSet.as_view({"get": "encounter"})),
     path('', include(router.urls)),
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from . import models
 
 # Create your views here.
@@ -22,3 +25,20 @@ class FoeEncountersViewSet(viewsets.ModelViewSet):
 class PartiesViewSet(viewsets.ModelViewSet):
     queryset = models.Party.objects.all()
     serializer_class = models.PartySerializer
+
+    @action(detail=True, methods=['get'])
+    def encounter(self, request, *args, **kwargs):
+        party = self.get_object()
+        party_serializer = self.get_serializer(party)
+
+        encounter_id = int(kwargs['encounter_id'])
+        encounter = models.Encounter.objects.get(pk=encounter_id)
+        encounter_serializer = models.EncounterSerializer(encounter, context={'request': request})
+
+        encounters = []
+        encounters.append(encounter_serializer.data)
+
+        response = party_serializer.data
+        response["encounters"] = encounters
+
+        return Response({"party": response})
